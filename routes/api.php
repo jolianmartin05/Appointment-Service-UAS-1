47c0bcaa8075b2fedf7b34c78c42541928777d52
jolianmartin05 <martinjolian@gmail.com>
2025-06-18 05:16:26 -0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\AppointmentController;
+
+Route::post('/appointments', [AppointmentController::class, 'store']);
+
+
+use Rebing\GraphQL\GraphQLController;
+
+Route::post('/graphql', [GraphQLController::class, 'query']);
